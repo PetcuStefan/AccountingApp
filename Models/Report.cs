81ad76af8edf812e2a87f205cf0c7864c4b5e70b
PetcuStefan <stefanitapetcu@gmail.com>
2025-09-03 21:28:55 +0300

    First commit

---
+++ Models/Report.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccountingApp.Models
+{
+    public class Report
+    {
+        public int id {  get; set; }
+        public double value { get; set; }
+        public string path { get; set; }
+    }
+}
---
+++ Services/DbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using AccountingApp.Models;
+using System.Collections.Generic;
+
+namespace AccountingApp.Services
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Report> Reports { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(
+                "Server=localhost;Database=MyAppDb;Trusted_Connection=True;");
+        }
+    }
+}
