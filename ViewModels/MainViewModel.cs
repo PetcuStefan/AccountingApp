0907ccc8c3b0b8d7528f40422bec3a9324e3b458
PetcuStefan <stefanitapetcu@gmail.com>
2025-09-08 17:02:29 +0300

    Added another table to the first window and some styling

--- ViewModels/MainViewModel.cs
+++ ViewModels/MainViewModel.cs
@@ -1,5 +1,4 @@
 ﻿using AccountingApp.Models;
-using AccountingApp.Services;
 using System;
 using System.Collections.ObjectModel;
 using System.Linq;
@@ -8,18 +7,29 @@ namespace AccountingApp.ViewModels
 {
     public class MainViewModel
     {
-        public ObservableCollection<Report> Reports { get; } = new ObservableCollection<Report>();
+        public ObservableCollection<Report> Costs { get; } = new ObservableCollection<Report>();
+        public ObservableCollection<Report> Revenues { get; } = new ObservableCollection<Report>();
 
         public MainViewModel()
         {
-            // If you want to load from DB (requires AppDbContext ready):
-            // using var db = new AppDbContext();
-            // var list = db.Reports.ToList();
-            // foreach (var r in list) Reports.Add(r);
+            // Sample data (replace with DB fetch)
+            var sampleReports = new[]
+            {
+                new Report { id = 1, value = -120.50, path = "http://example.com/cost1", date = DateOnly.FromDateTime(DateTime.Now.AddDays(-1)) },
+                new Report { id = 2, value = 300.75, path = "http://example.com/revenue1", date = DateOnly.FromDateTime(DateTime.Now) },
+                new Report { id = 3, value = -50.00, path = "http://example.com/cost2", date = DateOnly.FromDateTime(DateTime.Now.AddDays(-2)) },
+                new Report { id = 4, value = 1000.00, path = "http://example.com/revenue2", date = DateOnly.FromDateTime(DateTime.Now.AddDays(-5)) },
+            };
 
-            // Temporary sample data:
-            Reports.Add(new Report { id = 1, value = 123.45, path = "http://example.com/report", date = DateOnly.FromDateTime(DateTime.Now) });
+            foreach (var report in sampleReports)
+            {
+                if (report.value < 0)
+                    Costs.Add(report);
+                else if (report.value > 0)
+                    Revenues.Add(report);
+            }
         }
     }
 }
+
 
